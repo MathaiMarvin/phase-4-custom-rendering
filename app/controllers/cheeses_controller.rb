353842da7968a31b043df2f3418abea209119782
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-03-14 09:29:30 +0300

    Completed assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,21 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+
+    # WE can use the only flag to serialize what i want to be rendered
+    # WE can also use the except keyword which works the opposite of only excluding the stated 
+    render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
+
+    if cheese
     render json: cheese
+    else
+      render json: {error: "Cheese not found"}, status: 404
+    end
   end
 
 end
